e88d9aeca88d7207f9aa449714cd082c05d7a355
Pablo Strapasson <pablostrapasson98@gmail.com>
2024-09-17 17:16:30 -0300

    datasource-cli update

--- src/database/datasource-cli.ts
+++ src/database/datasource-cli.ts
@@ -1,12 +1,14 @@
+import { ConfigService } from '@nestjs/config';
 import { DataSource, DataSourceOptions } from 'typeorm';
 
+const config = new ConfigService();
 const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
-  host: 'postgres',
-  port: 5432,
-  username: 'root',
-  password: 'root',
-  database: 'db_shopper',
+  host: config.get<string>('DB_HOST'),
+  port: config.get<number>('DB_PORT'),
+  username: config.get<string>('DB_USERNAME'),
+  password: config.get<string>('DB_PASSWORD'),
+  database: config.get<string>('DB_NAME'),
   entities: [__dirname + '/../**/*.entity{.js,.ts}'],
   migrations: [__dirname + '/migrations/*.{js,ts}'],
 };
